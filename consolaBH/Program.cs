9261be7a61568b436b97d3a3c89e5315b59a223d
Lucio <lucioalema@gmail.com>
2019-06-03 20:51:11 -0300

    juan1

---
+++ consolaBH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consolaBH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+        }
+    }
+}
